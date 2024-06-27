badb6e2a5e9aba95883ecab472253baf339af0ae
RodrigoSantosG <rodrigosantosdev101@outlook.com>
2024-06-27 20:25:00 -0300

    configurando o google sheets

--- projeto/automacao_planilha.py
+++ projeto/automacao_planilha.py
@@ -5,4 +5,10 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pyautogui as pag
 
+# aqui sera a configuração do google sheets
+scope = ['https://spradshetts.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+gc = gspread.authorize(credentials)
+planilha = gc.open_by_key('ID da planilha')
+worksheet = planilha.worksheet('Nome da planilha')
 
