c4c6fec4bf14c0330d6a3fb789c094e621be2fdf
RodrigoSantosG <rodrigosantosdev101@outlook.com>
2024-06-27 20:04:22 -0300

    bibliotecas
    as bibliotecas utilizadas

---
+++ projeto/automacao_planilha.py
@@ -0,0 +1,8 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pyautogui as pag
+
+
